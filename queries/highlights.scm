ef5c448ac1fb30169f3ddd477ab20b29f4f97239
Shawon <mdmouinulhossainshawon@gmail.com>
2025-07-13 21:02:45 +0600

    refactor: Updated highlight queries

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,22 +1,48 @@
+;;; Inline elements.
+
 (italic) @markup.italic
 (bold) @markup.strong
-(code) @markup.raw
+((code) @markup.raw @nospell)
 
 (single_quote) @markup.link
-(double_quote) @markup.quote
+(double_quote) @markup.quote @nospell
 
-(issue_reference) @constant
+(issue_reference) @constant @nospell
 
 (number) @number
 ; (punctuation) @punctuation.delimiter
 
 (mention) @label
-(url) @string.special.url
+(url) @string.special.url @nospell
+
+
+;; Task specific highlights
 
 (task
   (label) @markup.strong)
 
 (task
+  (decorations
+    (topic) @markup.strong))
+
+(task
+  (label) @comment.note @nospell
+  (#any-of? @comment.note "PRAISE" "praise" "SUGGESTION" "suggestion" "THOUGHT" "thought" "note" "NOTE" "info" "INFO" "XXX"))
+
+(task
+  (label) @comment.warn @nospell
+  (#any-of? @comment.warn "NITPICK" "nitpick" "WARNING" "warning" "FIX" "fix" "HACK" "hack"))
+
+(task
+  (label) @comment.todo @nospell
+  (#any-of? @comment.todo "TODO" "todo" "TYPO" "typo" "WIP" "wip"))
+
+(task
+  (label) @comment.error @nospell
+  (#any-of? @comment.error "ISSUE" "issue" "ERROR" "error" "FIXME" "fixme" "DEPRECATED" "deprecated"))
+
+(task
+  (label)
   (decorations
     (topic) @markup.strong))
 
